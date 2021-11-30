a095ea5bd3f88a776f4cfec65ba019c7b3f4b681
kgithub-liquik <github+liquik@kenshoo.com>
2021-11-30 11:55:02 +0200

    project

---
+++ src/main/java/com/example/actions_release_creator/ActionsReleaseCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.actions_release_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsReleaseCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsReleaseCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/actions_release_creator/ActionsReleaseCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actions_release_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsReleaseCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
