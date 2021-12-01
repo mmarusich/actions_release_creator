e89ee20eba99d4f563c597a4dd0a40017bc3d26d
kgithub-liquik <github+liquik@kenshoo.com>
2021-12-01 18:15:09 +0200

    fix npe

--- src/main/java/com/example/actions_release_creator/ActionsReleaseCreatorApplication.java
+++ src/main/java/com/example/actions_release_creator/ActionsReleaseCreatorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActionsReleaseCreatorApplication {
 
 
+
     public static void main(String[] args) {
         SpringApplication.run(ActionsReleaseCreatorApplication.class, args);
     }
